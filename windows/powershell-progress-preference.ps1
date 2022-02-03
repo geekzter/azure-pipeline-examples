c6fa0fa1f2171befa0572c69b037b2378f7581c0
Eric van Wijk <eric@van-wijk.com>
2022-02-03 10:52:34 +0100

    Download 1GB file

--- windows/powershell-progress-preference.ps1
+++ windows/powershell-progress-preference.ps1
@@ -1,3 +1,7 @@
+param ( 
+    [parameter(Mandatory=$false)][string]$TempDirectory=$([system.io.path]::GetTempPath())
+) 
+
 $stopwatch = [system.diagnostics.stopwatch]::StartNew()
 for ($i = 1; $i -le 10000; $i++ )
 {
@@ -5,4 +9,8 @@ for ($i = 1; $i -le 10000; $i++ )
   Write-Progress -Activity "Test Progress" -Status "$percentComplete% Complete:" -PercentComplete $percentComplete
   Start-Sleep -Milliseconds 1
 }
+$stopwatch.Elapsed
+
+Invoke-Webrequest -Uri https://speed.hetzner.de/1GB.bin -OutFile (Join-Path $TempDirectory testfile) -UseBasicParsing
+
 $stopwatch.Elapsed
